2d384aa8aa944d852d5c922f6e51c743e91f6683
kisernl <kisernl@gmail.com>
2025-07-01 18:19:45 -0500

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          50: '#f9fafb',
+          900: '#111827',
+          950: '#030712',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
