98c28c83afdd5d9987d86b432f3927df759692b9
kisernl <kisernl@gmail.com>
2025-07-15 11:45:59 -0500

    refactor entire project to use starlight by astro

---
+++ astro.config.mjs
@@ -0,0 +1,43 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightThemeNova from 'starlight-theme-nova';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Compute',
+			logo: {
+				light: './src/assets/hv_main_logo_light.svg',
+				dark: './src/assets/hv_main_logo_dark.svg',
+			},
+			plugins: [starlightThemeNova({
+				nav: [
+					{
+					  label: 'Docs',
+					  href: '/guides/getting-started',
+					},
+					{
+					  label: 'Operator',
+					  href: '/operator',
+					},
+				],
+			  })],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
---
+++ src/content/docs/guides/getting-started.md
@@ -0,0 +1,6 @@
+---
+title: Getting Started
+description: A guide in my new Starlight docs site.
+---
+
+A getting started sectiong for our docs
